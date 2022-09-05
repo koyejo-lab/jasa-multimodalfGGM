12022f5d9f710844171a85d10232d52b86a81f26
karenta0317 <kt14@illinois.edu>
2022-09-05 15:03:41 -0700

    add script files

---
+++ code/tests/script/sample_size/run_model3_graph2.sh
@@ -0,0 +1,18 @@
+#please change the parameter in the bracket
+source activate [`conda environment`]
+cd [`path to run_model3`] 
+
+
+for i in 1 2 3 4 5 6 7 8 9 10 ;
+do
+    mkdir -p -- "../result_sample_batch/ss_${i}"  
+    python run_model3_N.py           --p 50  --type 'tridiag2' --s 4 --alpha 3 --filepath '../data_sample_batch/ss_${i}'  --id ${i}  --savepath '../result_sample_batch/ss_${i}' --lr_initb 0.01 --lr_a 0.0001 --lr_b 0.01
+    python run_model3_N_trueparam.py --p 50  --type 'tridiag2' --s 4 --alpha 3 --filepath '../data_sample_batch/ss_${i}'  --id ${i}  --savepath '../result_sample_batch/ss_${i}' --lr_initb 0.01 --lr_a 0.0001 --lr_b 0.01
+    
+    python run_model3_N.py           --p 100  --type 'tridiag2' --s 7 --alpha 3 --filepath '../data_sample_batch/ss_${i}'  --id ${i}  --savepath '../result_sample_batch/ss_${i}' --lr_initb 0.01 --lr_a 0.0001 --lr_b 0.01
+    python run_model3_N_trueparam.py --p 100  --type 'tridiag2' --s 7 --alpha 3 --filepath '../data_sample_batch/ss_${i}'  --id ${i}  --savepath '../result_sample_batch/ss_${i}' --lr_initb 0.01 --lr_a 0.0001 --lr_b 0.01
+
+    python run_model3_N.py           --p 150  --type 'tridiag2' --s 4 --alpha 3 --filepath '../data_sample_batch/ss_${i}'  --id ${i}  --savepath '../result_sample_batch/ss_${i}' --lr_initb 0.01 --lr_a 0.0001 --lr_b 0.01
+    python run_model3_N_trueparam.py --p 150  --type 'tridiag2' --s 4 --alpha 3 --filepath '../data_sample_batch/ss_${i}'  --id ${i}  --savepath '../result_sample_batch/ss_${i}' --lr_initb 0.01 --lr_a 0.0001 --lr_b 0.01
+        
+done
