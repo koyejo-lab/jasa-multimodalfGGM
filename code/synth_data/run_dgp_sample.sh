cd7e01325809b8357a41bea38b08e04a4d51b355
karenta0317 <kt14@illinois.edu>
2023-04-05 22:29:33 -0700

    for merge

--- code/synth_data/run_dgp_sample.sh
+++ code/synth_data/run_dgp_sample.sh
@@ -1,9 +1,8 @@
-for i in  1 2 3 4 5 6 7 8 9 10;
+for i in  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20;
 do
     mkdir -p -- "../../data/data_sample_batch/ss_${i}"  
     Rscript datageneration_sample.R cov_name="tridiag1" path="../../data/data_sample_batch/ss_${i}"
     Rscript datageneration_sample.R cov_name="tridiag2" path="../../data/data_sample_batch/ss_${i}"
     Rscript datageneration_sample.R cov_name="tridiag3" path="../../data/data_sample_batch/ss_${i}"
-    Rscript datageneration_sample.R cov_name="power"    path="../../data/data_sample_batch/ss_${i}"
-
+   
 done
